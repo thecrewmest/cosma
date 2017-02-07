9df203e56a3c87864b86b33b7688ac87ec553c36
muriithiKabogo <dmuriithi.k@gmail.com>
2017-02-07 08:40:15 +0000

    migratedatabase

--- db/migrate/20170206132452_create_privileges.rb
+++ db/migrate/20170206132452_create_privileges.rb
@@ -2,7 +2,7 @@ class CreatePrivileges < ActiveRecord::Migration[5.0]
   def change
     create_table :privileges do |t|
       t.integer :user_id
-      t.Boolean :create_right
+      t.boolean :create_right
       t.boolean :read_right
       t.boolean :update_right
       t.boolean :delete_right
