0b1251b5425cf854ac5be8ca2ea2df506e7b7dca
thecrewmest <thecrewmest@gmail.com>
2017-02-12 06:16:55 +1200

    Creates the layout for the static page and dashboard
    I created the entire site layouts and dashboard

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,13 @@
+//= require site/modernizr-2.8.3.min
+//= require site/jquery.stellar.min
+//= require site/owl.carousel.min
+//= require site/jquery.mixitup
+//= require site/venobox.min
+//= require site/scrolltopcontrol
+//= require site/form-contact
+//= require site/jquery.appear
+//= require site/wow.min
+//= require site/hero-slider
+//= require site/scripts
+
+
